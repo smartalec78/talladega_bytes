593bf4c3df8640cc7e7b33b78d922a89dbe8a976
Alec Bueing <alecb78@gmail.com>
2020-11-06 17:16:49 -0700

    Commit of what is now the hoppedUpOnMtnDewV3 reward model.

--- mtnDew.py
+++ mtnDew.py
@@ -1,4 +1,8 @@
 import math
+from math import cos as cos
+from math import sin as sin
+from math import pow as pow
+from math import pi as pi
 
 def reward_function(params):
     '''
@@ -8,64 +12,51 @@ def reward_function(params):
     # Read input parameters
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-    
-    # list out all available params
     all_wheels_on_track = params['all_wheels_on_track'] #: Boolean,        # flag to indicate if the agent is on the track
     #locX = paramsparams['x'] #: float,                            # agent's x-coordinate in meters
     #locY = params['y'] #: float,                            # agent's y-coordinate in meters
-    #closest_objects = params['closest_objects'] #: [int, int],         # zero-based indices of the two closest objects to the agent's current position of (x, y).
-    #closest_waypoints = params['closest_waypoints'] #: [int, int],       # indices of the two nearest waypoints.
-    distance_from_center = params['distance_from_center'] #: float,         # distance in meters from the track center 
     is_crashed = params['is_crashed'] #: Boolean,                 # Boolean flag to indicate whether the agent has crashed.
     is_left_of_center = params['is_left_of_center'] #: Boolean,          # Flag to indicate if the agent is on the left side to the track center or not. 
     is_offtrack = params['is_offtrack'] #: Boolean,                # Boolean flag to indicate whether the agent has gone off track.
     is_reversed = params['is_reversed'] #: Boolean,                # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
     heading = params['heading'] #: float,                      # agent's yaw in degrees
-    #objects_distance = params['objects_distance'] #: [float, ],         # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
-    #objects_heading = params['objects_heading'] #: [float, ],          # list of the objects' headings in degrees between -180 and 180.
-    #objects_left_of_center = params['objects_left_of_center'] #: [Boolean, ], # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
-    #objects_location = params['objects_location'] #: [(float, float),], # list of object locations [(x,y), ...].
-    #objects_speed = params['objects_speed'] #: [float, ],            # list of the objects' speeds in meters per second.
+
     progress = params['progress'] #: float,                     # percentage of track completed
     speed = params['speed'] #: float,                        # agent's speed in meters per second (m/s)
     steering_angle = params['steering_angle'] #: float,               # agent's steering angle in degrees
     #steps = params['steps'] #: int,                          # number steps completed
     #track_length = params['track_length'] #: float,                 # track length in meters.
-    #track_width = params['track_width'] #: float,                  # width of the track
     #waypoints = params['waypoints'] #: [(float, float), ]        # list of (x,y) as milestones along the track center
     
+    velocity_vector = [math.cos(heading*math.pi/180), math.sin(heading*math.pi/180)]
+    persistent_vel_vector = velocity_vector
     # assume no reward at first
     reward = 0
     
     # increase points for getting further down the track
     # if you make it all the way, 100 pts
-    reward += progress
-    
+    #reward += progress - last_progress
+    #last_progress = progress # so apparently you can't have persistent variables here
     
     
     # slight penalty if not all wheels on the track
     reward -= 5 * float(not all_wheels_on_track)
     
-    
     # larger penalty if completely off track
     reward -= 20 * float(is_offtrack)
-    
     
     # another large penalty if crashed
     reward -= 25 *float(is_crashed)
     reward -= 25 * float(is_reversed)
     
-    
     # penalize for steering too much while going fast
-    reward += speed*math.cos(steering_angle*math.pi/180)
+    reward += math.pow(speed,-math.cos(2*steering_angle*math.pi/180))
+    # spd*cos(2*ang*pi/180)^2 + (cos(2*ang*pi/180)*cos(2*a*pi/180)^(2*spd))/(spd^(1/2) + 1)
+    spd = speed
+    ang2 = steering_angle * 2*pi/180
+    reward += spd*(pow(cos(ang2),2)) + (cos(ang2)*pow(cos(ang2),2*spd))/(pow(spd,0.5)+1)
     
-    reward += math.pow(10,-distance_from_center)
     
-    
+    reward += pow(10,-distance_from_center)
     
     return float(reward)
